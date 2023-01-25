a4a25f6f3ec5ec7d3bc3a59750ce8ab6aea13eb4
Ashis Chaudhary <ashischaudhary959@gmail.com>
2023-01-26 00:05:10 +1100

    add dummy pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to HOME page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fuga cumque
+        labore odit natus impedit totam illum dolore commodi quam eveniet animi
+        aliquam suscipit repudiandae, ut minus possimus quia modi? Molestias,
+        labore blanditiis! A voluptate fugiat quo vitae minus voluptatem tenetur
+        magni praesentium deleniti sapiente pariatur iusto error voluptas, ad
+        atque nesciunt maxime vel hic quisquam veritatis ducimus? Reprehenderit,
+        ipsa. Totam blanditiis ullam assumenda iusto labore similique tempora
+        enim culpa dolorem corrupti aspernatur unde libero architecto suscipit,
+        maxime nihil itaque doloribus cum, qui illum quidem? Accusamus, quisquam
+        quia. Fugit, nobis. Optio repellendus dolor, et corporis autem officiis
+        magni quasi dolore saepe! Ad vel, quo ipsa modi sint perferendis neque
+        similique, vero ab cupiditate voluptatibus officiis, voluptatum ipsam!
+        Quas, similique mollitia? Dolorem temporibus officiis vero illum
+        eligendi. Id facilis provident ad at ipsum voluptates magnam reiciendis
+        eligendi quisquam, ullam facere ratione, placeat quae itaque sunt
+        aliquid quaerat a unde fuga! Provident? Error, neque obcaecati
+        assumenda, quod harum culpa eum quidem dolorem molestiae non accusamus
+        fugit nobis placeat maiores nemo veniam, rem tempora! Quidem delectus
+        praesentium quis! Molestiae eos odit eius quibusdam. Itaque impedit
+        incidunt minus saepe ducimus exercitationem deserunt quae, repellendus
+        delectus libero? Non alias repellendus dolorum labore sit earum,
+        asperiores, ipsum, porro sapiente nihil aut natus temporibus quidem
+        numquam. Quibusdam? Magni molestias qui minus iste minima sit veniam
+        quisquam cupiditate at explicabo sint nostrum vero sunt inventore fuga
+        deserunt et repellendus vitae, quod temporibus. Maiores repudiandae ut
+        veniam dignissimos aperiam! Nobis a voluptate doloribus facilis
+        accusamus, eos non necessitatibus libero! Maxime a eos error veritatis
+        quia dicta, quas dignissimos ratione ad excepturi eius distinctio
+        tempora aliquid odio porro praesentium eligendi. Error nisi hic libero.
+        Fugit, illum. Assumenda magni laborum nostrum? Libero suscipit, illo
+        exercitationem accusantium amet quo commodi aut voluptates saepe
+        cupiditate error in. Quae facilis provident at quisquam officia!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
